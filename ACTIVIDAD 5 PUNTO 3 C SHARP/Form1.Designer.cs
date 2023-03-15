3095e25d80495bcd976f545eb0802f3b410f5d4c
JoseFabela <jfabela522@gmail.com>
2023-03-14 23:25:15 -0600

    Agregar archivos de proyecto.

---
+++ ACTIVIDAD 5 PUNTO 3 C SHARP/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace ACTIVIDAD_5_PUNTO_3_C_SHARP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnIdentificar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnIdentificar
+            // 
+            this.btnIdentificar.Location = new System.Drawing.Point(393, 94);
+            this.btnIdentificar.Name = "btnIdentificar";
+            this.btnIdentificar.Size = new System.Drawing.Size(75, 23);
+            this.btnIdentificar.TabIndex = 0;
+            this.btnIdentificar.Text = "button1";
+            this.btnIdentificar.UseVisualStyleBackColor = true;
+            this.btnIdentificar.Click += new System.EventHandler(this.btnIdentificar_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnIdentificar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnIdentificar;
+    }
+}
+
---
+++ ACTIVIDAD 5 PUNTO 3 C SHARP/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ACTIVIDAD_5_PUNTO_3_C_SHARP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+  
+        private void btnIdentificar_Click_1(object sender, EventArgs e)
+        {
+            // Arreglo unidimensional
+            int[] numeros1D = { 2, 4, 6, 8, 10 };
+
+            MessageBox.Show("Arreglo unidimensional\n\n" +
+                            $"Cantidad de elementos: {numeros1D.Length}\n" +
+                            $"Elemento más grande: {numeros1D.Max()}\n" +
+                            $"Elemento más pequeño: {numeros1D.Min()}");
+
+            // Arreglo bidimensional
+            int[,] numeros2D = { { 1, 2 }, { 3, 4 }, { 5, 6 } };
+
+            MessageBox.Show("Arreglo bidimensional\n\n" +
+                            $"Cantidad de filas: {numeros2D.GetLength(0)}\n" +
+                            $"Cantidad de columnas: {numeros2D.GetLength(1)}\n" +
+                            $"Elemento en la fila 2, columna 1: {numeros2D[1, 0]}\n" +
+                            $"Elemento en la fila 3, columna 2: {numeros2D[2, 1]}");
+
+            // Arreglo tridimensional
+            int[,,] numeros3D = { { { 1, 2, 3 }, { 4, 5, 6 } }, { { 7, 8, 9 }, { 10, 11, 12 } } };
+
+            MessageBox.Show("Arreglo tridimensional\n\n" +
+                            $"Cantidad de páginas: {numeros3D.GetLength(0)}\n" +
+                            $"Cantidad de filas: {numeros3D.GetLength(1)}\n" +
+                            $"Cantidad de columnas: {numeros3D.GetLength(2)}\n" +
+                            $"Elemento en la página 1, fila 1, columna 3: {numeros3D[0, 0, 2]}\n" +
+                            $"Elemento en la página 2, fila 2, columna 2: {numeros3D[1, 1, 1]}");
+        }
+    }
+}
+
+
